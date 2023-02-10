d1c2881442f793b4531ca29e89daeb91b603847a
Ajay Sivan <ajayesivan@gmail.com>
2023-02-10 23:08:37 +0530

    color-picker: Layout

--- src/quo2/components/colors/color_picker/style.cljs
+++ src/quo2/components/colors/color_picker/style.cljs
@@ -1,44 +1,28 @@
-(ns quo2.components.colors.color-picker.style)
+(ns quo2.components.colors.color-picker.style
+  (:require [quo2.foundations.colors :as colors]))
 
 (def color-picker-container
-  {
-   :background-color :yellow})
+  {:max-width 338
+   :height 106
+   :justify-content :space-between})
 
-(def heighlight-left
-  {:background-color :red
-   :width 24
-   :height 48})
+(def color-picker-row
+  {:flex-direction :row
+   :justify-content :space-between})
 
-(def heighlight-right
-  {:background-color :green
-   :width 24
-   :height 48})
-
-(def color
-  {:background-color :purple
-   :width 40
-   :height 40
-   :border-radius 20
-   :position :absolute
-   :top 4
-   :left 4})
-
-(def color-button
-  {
-   :overflow :hidden
-   :width 48
-   :height 48
-   :border-radius 24
-   :flex-direction :row})
-
-(def color-button2
+(defn color-button []
   {:width 48
    :height 48
-   :border-radius 24
-   :background-color :red
    :border-width 4
+   :border-radius 24
    :transform [{:rotate "45deg"}]
-   :border-bottom-color :green
-   :border-top-color :orange
-   :border-left-color :green
-   :border-right-color :orange})
+   :border-top-color (colors/alpha "#2A4AF5" 0.2)
+   :border-right-color (colors/alpha "#2A4AF5" 0.2)
+   :border-bottom-color (colors/alpha "#2A4AF5" 0.4)
+   :border-left-color (colors/alpha "#2A4AF5" 0.4)})
+
+(defn color-circle []
+  {:width 40
+   :height 40
+   :background-color "#2A4AF5"
+   :border-radius 20})
--- src/quo2/components/colors/color_picker/view.cljs
+++ src/quo2/components/colors/color_picker/view.cljs
@@ -1,17 +1,29 @@
-(ns quo2.components.colors.color-picker.view 
+(ns quo2.components.colors.color-picker.view
   (:require [react-native.core :as rn]
             [quo2.components.colors.color-picker.style :as style]))
 
-(defn color []
-  [rn/view {:style style/color-button}
-   [rn/view {:style style/heighlight-left}]
-   [rn/view {:style style/heighlight-right}]
-   [rn/view {:style style/color}]])
+(def colors [{:name "blue" :color "#2A4AF5" :key 1}
+             {:name "purple" :color "#7140FD" :key 1}
+             {:name "army" :color "#216266" :key 1}
+             {:name "turquoise" :color "#2A799B" :key 1}
+             {:name "sky" :color "#1992D7" :key 1}
+             {:name "magenta" :color "#EC266C" :key 1}
+             {:name "yellow" :color "#F6B03C" :key 1}
+             {:name "flamingo" :color "#F66F8F" :key 1}
+             {:name "copper" :color "#CB6256" :key 1}
+             {:name "camel" :color "#C78F67" :key 1}
+             {:name "orange" :color "#FF7D46" :key 1}
+             {:name "fengshui" :color "#09101C" :key 1}])
 
-(defn color2 []
-  [rn/view {:style style/color-button2}])
+(defn color-item []
+  [rn/touchable-opacity {:style (style/color-button) :on-press #(print "Clicked me!!")}
+   [rn/view {:style (style/color-circle)}]])
 
 (defn view []
   [rn/view {:style style/color-picker-container}
-   [color2]
-   [color]])
+   [rn/view {:style style/color-picker-row}
+    (for [color (subvec colors 0 6)]
+      [color-item])]
+   [rn/view {:style style/color-picker-row}
+    (for [color (subvec colors 6 12)]
+      [color-item])]])
