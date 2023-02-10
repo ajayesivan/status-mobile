c5dffb8afbb5a598444ba6fc947f2d49fc0141e3
Ajay Sivan <ajayesivan@gmail.com>
2023-02-10 20:32:38 +0530

    color-picker: Color Picker Item Design

--- src/quo2/components/colors/color_picker/style.cljs
+++ src/quo2/components/colors/color_picker/style.cljs
@@ -1,4 +1,44 @@
 (ns quo2.components.colors.color-picker.style)
 
 (def color-picker-container
-  {:background-color :red})
+  {
+   :background-color :yellow})
+
+(def heighlight-left
+  {:background-color :red
+   :width 24
+   :height 48})
+
+(def heighlight-right
+  {:background-color :green
+   :width 24
+   :height 48})
+
+(def color
+  {:background-color :purple
+   :width 40
+   :height 40
+   :border-radius 20
+   :position :absolute
+   :top 4
+   :left 4})
+
+(def color-button
+  {
+   :overflow :hidden
+   :width 48
+   :height 48
+   :border-radius 24
+   :flex-direction :row})
+
+(def color-button2
+  {:width 48
+   :height 48
+   :border-radius 24
+   :background-color :red
+   :border-width 4
+   :transform [{:rotate "45deg"}]
+   :border-bottom-color :green
+   :border-top-color :orange
+   :border-left-color :green
+   :border-right-color :orange})
--- src/quo2/components/colors/color_picker/view.cljs
+++ src/quo2/components/colors/color_picker/view.cljs
@@ -2,6 +2,16 @@
   (:require [react-native.core :as rn]
             [quo2.components.colors.color-picker.style :as style]))
 
+(defn color []
+  [rn/view {:style style/color-button}
+   [rn/view {:style style/heighlight-left}]
+   [rn/view {:style style/heighlight-right}]
+   [rn/view {:style style/color}]])
+
+(defn color2 []
+  [rn/view {:style style/color-button2}])
+
 (defn view []
   [rn/view {:style style/color-picker-container}
-   [rn/text "Colors/Color Picker -1"]])
+   [color2]
+   [color]])
